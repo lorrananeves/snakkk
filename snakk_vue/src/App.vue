68361489e5b511ec6b7725cb9ed01fb2d77ecd6a
Lorrana <lorranacaneves@gmail.com>
2023-02-10 06:25:15 -0300

    construido casca do projeto com docker ja imprementado

---
+++ snakk_vue/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <nav class="navbar is-dark pt pb-4">
+            <div class="navbar-brand">
+                <router-link to="/" class="navbar-item is-size-4"><strong>Snakk</strong></router-link>
+            </div>
+
+            <div class="navbar-menu">
+                <div class="navbar-end">
+                    <router-link to="/about" class="navbar-item">About</router-link>
+
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link to="/signup" class="button is-primary"><strong>Sign Up</strong></router-link>
+                            <router-link to="/login" class ="button is-light">Login</router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <router-view/>
+
+        <footer>
+            <div class="has-text-centered">Copyright (c) 2023</div>
+        </footer>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+
+.navbar.is-dark {
+    background-color: #222;
+}
+
+body {
+    background: #333;
+}
+
+.landing-page {
+    min-height: 100vh;
+
+    .content {
+        color: #aaa;
+
+        h1 {
+            color: #eee
+        }
+
+        h2, h3 {
+            font-weight: 200;
+            color:#bbb;
+        }
+
+        label {
+            font-size: 13px;
+            color: #aaa;
+        }
+
+        hr {
+            background: #444;
+        }
+
+        @media (max-width: 767px) {
+            .reverse-coluns {
+                flex-direction: column-reverse;
+                display: flex;
+            }
+        }
+    }
+}
+
+</style>
+
---
+++ snakk_vue/src/views/HomeView.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="home">
+    <section class="section landing-page">
+      <div class="content">
+        <section class="hero is-medium">
+          <div class="hero-body has-text-centered">
+            <h1 class="is-size-1"> Welcome to Snakk</h1>
+            <h2 class="is-size-4">Bring your team together</h2>
+          </div>
+        </section>
+
+        <section class="hero is-small">
+          <div class="hero-body">
+            <div class="container">
+              <div class="columns is-centered">
+                <div class="column is-4 is-offset-1">
+                  <figure class="image">
+                    <img src="@/assets/numero1.png">
+                  </figure>
+                </div>
+                
+                <div class="column is5 is-offset-1">
+                  <h2 class="title is-2 mb-6"> Group Chats</h2>
+
+                  <h3 class="subtitle is-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="hero is-small">
+          <div class="hero-body">
+            <div class="container">
+              <div class="columns is-centered reverse-columns">
+                
+                <div class="column is5 is-offset-1">
+                  <h2 class="title is-2 mb-6"> Group Chats</h2>
+
+                  <h3 class="subtitle is-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h3>
+                </div>
+
+                <div class="column is-4 is-offset-1">
+                  <figure class="image">
+                    <img src="@/assets/numero1.png">
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="hero is-small">
+          <div class="hero-body">
+            <div class="container">
+              <div class="columns is-centered">
+                <div class="column is-4 is-offset-1">
+                  <figure class="image">
+                    <img src="@/assets/numero1.png">
+                  </figure>
+                </div>
+                
+                <div class="column is5 is-offset-1">
+                  <h2 class="title is-2 mb-6"> Group Chats</h2>
+
+                  <h3 class="subtitle is-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
