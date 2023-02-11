24a7663f970126ab2412a3d279fbd9b28c2ffcba
Lorrana <lorranacaneves@gmail.com>
2023-02-10 21:44:46 -0300

    construido chat room

--- snakk_django/snakk_django/urls.py
+++ snakk_django/snakk_django/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),+
+    path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/userprofile/', include('userprofile.urls')),
     path('api/v1/teams/', include('team.urls')),
 ]
